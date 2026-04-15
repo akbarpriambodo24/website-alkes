cfb24036afd3de374d05c5b74ad7531f6efd0ab5
Akbar <akbarpriambodo24@gmail.com>
2026-04-15 20:09:07 +0700

    chore: add lapanak to alphakirana redirection for SEO

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,21 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: '(www\\.)?lapanak\\.co\\.id',
+          },
+        ],
+        destination: 'https://www.alphakirana.co.id/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
